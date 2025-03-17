679255cee87a52a8fc487760a027eb781bdc2beb
matt598 <mattsaueroa@gmail.com>
2025-03-16 20:06:39 -0500

    Updating readme

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
-
+import Link from "next/link"
 
 export default function About() {
   return (
-      <div>About</div>)
+      <div><h1>About</h1>
+        <div>This is just a proof of concept site for my P4A Website Redesign Submission, checkout the repo <Link href="https://github.com/matt598/project-for-tuawesome-ui">here</Link></div>
+      </div>)
 }
